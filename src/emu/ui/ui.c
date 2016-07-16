5dc95f4c40469efe81042e91a248ac3760d3ee63
Benjamin Hodgetts <ben@xnode.org>
2016-07-16 16:03:55 +0100

    Update var names
    To match the new ones used in the config.

--- src/emu/ui/ui.c
+++ src/emu/ui/ui.c
@@ -308,7 +308,7 @@ UINT32 ui_manager::set_handler(ui_callback callback, UINT32 param)
 
 #ifdef OSD_RETRO
 	extern bool hide_nagscreen;
-	extern bool hide_gameinfo;
+	extern bool hide_infoscreen;
 	extern bool hide_warnings;
 #endif
 
@@ -333,7 +333,7 @@ void ui_manager::display_startup_screens(bool first_time, bool show_disclaimer)
 	#ifdef OSD_RETRO
 		if(hide_nagscreen)
 			show_disclaimer = FALSE;
-		if(hide_gameinfo)
+		if(hide_infoscreen)
 			show_gameinfo = FALSE;
 		if(hide_warnings)
 			show_warnings = FALSE;
