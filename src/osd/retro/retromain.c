0e5c196d37afe9e6422e41b51eff5f9cb6524674
sergiobenrocha2 <sergiobenrocha2@gmail.com>
2016-12-02 03:03:35 -0200

    Swap X and Y in Dungeons & Dragons: Shadow over Mystara (make it use the same layout of Tower of Doom).

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -1209,6 +1209,20 @@ static void Input_Binding(running_machine &machine)
       Buttons_mapping[5]=RETROPAD_R;
    }
    else if (
+              (core_stricmp(machine.system().name, "ddsom") == 0) ||
+              (core_stricmp(machine.system().parent, "ddsom") == 0)
+           )
+   {
+	  /* Capcom CPS-2: Dungeons & Dragons: Shadow over Mystara (same layout of ddtod) */
+
+      Buttons_mapping[0]=RETROPAD_A;
+      Buttons_mapping[1]=RETROPAD_B;
+      Buttons_mapping[2]=RETROPAD_Y;
+      Buttons_mapping[3]=RETROPAD_X;
+      Buttons_mapping[4]=RETROPAD_L;
+      Buttons_mapping[5]=RETROPAD_R;
+   }
+   else if (
               (core_stricmp(machine.system().parent, "aof") == 0) ||
               (core_stricmp(machine.system().parent, "aof2") == 0) ||
               (core_stricmp(machine.system().parent, "aof3") == 0) ||
