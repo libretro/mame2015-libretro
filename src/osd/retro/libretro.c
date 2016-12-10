d1129bac0946755314c65cdb0596c7e7c71128ee
Gregor Richards <hg-yff@gregor.im>
2016-12-09 19:46:30 -0500

    Report git version with library_version

--- src/osd/retro/libretro.c
+++ src/osd/retro/libretro.c
@@ -412,7 +412,10 @@ void retro_get_system_info(struct retro_system_info *info)
    info->library_name     = "MAME 2014";
 #endif
 
-   info->library_version  = "0.159";
+#ifndef GIT_VERSION
+#define GIT_VERSION ""
+#endif
+   info->library_version  = "0.159" GIT_VERSION;
    info->valid_extensions = "zip|chd|7z";
    info->need_fullpath    = true;
    info->block_extract    = true;
